365f9cd0d272302ab2919466bdfdd05368692998
Emil Hacklin <36034657+EmilHacklin@users.noreply.github.com>
2018-02-15 21:05:40 +0100

    15/2-2018 Collision Test Stable

---
+++ SFML Test/SFML Project/Collisions_test.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+#include<vector>
+#include<SFML\System.hpp>
+#include<SFML\Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+void eventTest(RenderWindow &window, Sprite &sprite, Sprite &obstacle)
+{
+	Event event;
+	window.pollEvent(event);
+	Vector2u windowSize = window.getSize();
+	Vector2f spritePos = sprite.getPosition();
+	FloatRect spriteBoundingBox = sprite.getGlobalBounds();
+	FloatRect obstacleBoundingBox = obstacle.getGlobalBounds();
+	switch (event.type)
+	{
+	case Event::Closed:
+		window.close();
+		break;
+	case Event::KeyPressed:
+		if (event.key.code == Keyboard::Left)
+		{
+			if (spritePos.x > 0)
+			{
+				sprite.move(-5, 0);
+			}
+		}
+		else if (event.key.code == Keyboard::Right)
+		{
+			if ((spritePos.x + spriteBoundingBox.width) < windowSize.x)
+			{
+				sprite.move(5, 0);
+			}
+		}
+		else if (event.key.code == Keyboard::Up)
+		{
+			if (spritePos.y > 0)
+			{
+				sprite.move(0, -5);
+			}
+		}
+		else if (event.key.code == Keyboard::Down)
+		{
+			if ((spritePos.y + spriteBoundingBox.height) < windowSize.y)
+			{
+				sprite.move(0, 5);
+			}
+		}
+		if (spriteBoundingBox.intersects(obstacleBoundingBox))
+		{
+			cout << "Collision has occurred!" << endl;
+			
+			window.display();
+			window.clear(Color::Black);
+			window.draw(sprite);
+			window.draw(obstacle);
+
+			if (event.key.code == Keyboard::Left)
+			{
+				sprite.move(20, 0);
+			}
+			else if (event.key.code == Keyboard::Right)
+			{
+				sprite.move(-20, 0);
+			}
+			else if (event.key.code == Keyboard::Up)
+			{
+				sprite.move(0, 20);
+			}
+			else if (event.key.code == Keyboard::Down)
+			{
+				sprite.move(0, -20);
+			}
+		}
+		break;
+	default:// we don't process other types of events
+		break;
+	}
+}
+ 
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "My window", Style::Close);
+	Texture red, green;
+	Vector2u sizeOfRed, sizeOfGreen;
+	Sprite redBox, greenBox;
+
+
+	if (!red.loadFromFile("../Resources/Red.png") || !green.loadFromFile("../Resources/Green.png"))
+	{
+		return -1;
+	}
+
+	sizeOfRed = red.getSize();
+	sizeOfGreen = red.getSize();
+	
+	redBox.setTexture(red);
+	greenBox.setTexture(green);
+
+	redBox.setPosition((float)(400 - (sizeOfRed.x / 2)), (float)(300 - (sizeOfRed.y / 2)));
+	greenBox.setPosition((float)(400 - (sizeOfGreen.x / 2)), (float)(100 - (sizeOfGreen.y / 2)));
+
+	while (window.isOpen())
+	{
+		window.clear(Color::Black);
+
+		window.draw(redBox);
+		window.draw(greenBox);
+
+		eventTest(window, greenBox, redBox);
+
+		window.display();
+	}
+
+	return 0;
+}
